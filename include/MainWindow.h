96dec5e1c4763c2ba1c4a5e1b8b1f89da6c734ee
alskondr <alskondr@yandex.ru>
2017-10-24 18:56:04 +0300

    Changed GUI

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -19,14 +19,24 @@ public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
 
+  /*!
+   * \brief Запись датчиков в лог
+   */
   Q_SLOT void printSensorsLog();
 
+  /*!
+   * \brief Обработчик нажатия на клавишу выбора директории с проектом
+   */
   Q_SLOT void pushProjectDirButton();
+
+  /*!
+   * \brief Обработчик нажатия на клавишу выбора файла с логом датчиков
+   */
   Q_SLOT void pushSensorsLogButton();
 
 private:
-  Ui::MainWindow *ui;
-  ProjectDirModel m_projectDirModel;
+  Ui::MainWindow *ui; //! Форма главного окна
+  ProjectDirModel m_projectDirModel; //! Модель директории с проектом
 }; // class MainWindow
 
 #endif // MAINWINDOW_H
